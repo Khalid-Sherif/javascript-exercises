da3aa6a37270ba1a81d926c4139b7ff9ffb42f5d
Khaled Sherif <khsherif13@gmail.com>
2023-01-26 18:27:21 +0300

    exercise 10 added

---
+++ es-10/exercise.js
@@ -0,0 +1,8 @@
+const firstYearCompleted = true;
+const yearsCompleted = 1;
+
+yearsCompleted == firstYearCompleted
+yearsCompleted === firstYearCompleted
+
+// '==' operator will convert the data type of the variable and check the equality of the values
+// '===' operator will NOT convert the data type of the variable and check the equality
