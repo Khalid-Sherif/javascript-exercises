b06ae6a7b2ce8c3de817f3b5793aacdf90283e20
Khaled Sherif <khsherif13@gmail.com>
2023-01-26 17:53:05 +0300

    exercise 8 added

---
+++ es-8/exercise.js
@@ -0,0 +1,16 @@
+let variable = true;
+
+console.log(variable);
+console.log(typeof (variable));
+// ----------------------------
+variable = Number(variable);
+console.log(variable);
+console.log(typeof (variable));
+// ----------------------------
+variable = String(variable)
+console.log(variable);
+console.log(typeof (variable));
+// ----------------------------
+variable = Boolean(variable)
+console.log(variable);
+console.log(typeof (variable));
