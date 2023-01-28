528f5e704b9783c250e700036ec7becabaafecb0
Khaled Sherif <khsherif13@gmail.com>
2023-01-28 12:16:52 +0300

    exercise 23 added

---
+++ es-23/exercise.js
@@ -0,0 +1,14 @@
+const roomType = "Master Suite Room";
+
+switch (roomType) {
+    case "Basic Room":
+        console.log("50€");
+        break;
+
+    case "Junior Suite Room":
+        console.log("80€");
+        break;
+    case "Master Suite Room":
+        console.log("100€");
+        break;
+}
