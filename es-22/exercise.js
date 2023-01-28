28a7b42c094cac21612b5871e6392bb77097a33e
Khaled Sherif <khsherif13@gmail.com>
2023-01-28 12:02:41 +0300

    exercise 22 added

---
+++ es-22/exercise.js
@@ -0,0 +1,13 @@
+const primitive = 5;
+
+switch (typeof (primitive)) {
+    case "number":
+        console.log(primitive + " : " + typeof (primitive));
+        break;
+    case "string":
+        console.log(primitive + " : " + typeof (primitive));
+        break;
+    case "boolean":
+        console.log(primitive + " : " + typeof (primitive));
+        break;
+}
