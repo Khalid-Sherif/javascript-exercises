eb0327d67fa807c6cdd470c5054174f5434aca81
Khaled Sherif <khsherif13@gmail.com>
2023-01-28 20:33:39 +0300

    exercise 32 added

---
+++ es-32/exercise.js
@@ -0,0 +1,13 @@
+function sayHelloName(callback) {
+    setTimeout(function () {
+        console.log("Hello ");
+        callback();
+    }, 1000);
+
+}
+
+function printName() {
+    console.log("Cosimo");
+}
+
+sayHelloName(printName);
