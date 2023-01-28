640411a3d5a8551ada43a8faeee0d89321571337
Khaled Sherif <khsherif13@gmail.com>
2023-01-28 21:01:27 +0300

    exercise 35 added

---
+++ es-35/exercise.js
@@ -0,0 +1,18 @@
+function canPlay() {
+  let sport = " Football";
+  let personName = "Cosimo";
+
+  // if (true) {
+  //   let personName = "Cosimo";
+  // }
+
+  console.log(personName + sport);
+}
+
+canPlay();
+
+/*
+When defining a variable inside an if statement, it is considered a local
+scope which is only defined in the if statement block and can't be called
+from the outside.
+*/
