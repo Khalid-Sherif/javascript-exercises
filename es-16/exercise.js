b6e92365de6c42fd9c787eaddb62284fec9acd0b
Khaled Sherif <khsherif13@gmail.com>
2023-01-28 09:57:37 +0300

    exercise 16 added

---
+++ es-16/exercise.js
@@ -0,0 +1,9 @@
+let isSunny = true,
+    isHomeworkCompleted = true;
+
+console.log(isSunny && isHomeworkCompleted ? "Jesse can go out" : "Jesse stays home");
+
+
+isSunny = false;
+
+console.log(isSunny && isHomeworkCompleted ? "Jesse can go out" : "Jesse stays home");
