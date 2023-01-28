963b63b258bba437da2344fe854e2997c61a79aa
Khaled Sherif <khsherif13@gmail.com>
2023-01-28 08:55:15 +0300

    exercise 15 added

---
+++ es-15/exercise.js
@@ -0,0 +1,11 @@
+let age = 18;
+
+let canDrive = age >= 18 ? "You can drive a car" : "You are too young to drive";
+
+console.log(canDrive);
+
+let age2 = 17;
+
+let canDrive2 = age2 >= 18 ? "You can drive a car" : "You are too young to drive";
+
+console.log(canDrive2);
