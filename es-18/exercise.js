ae863424fa86d1e6457962ce3378da5bc0e6d5b8
Khaled Sherif <khsherif13@gmail.com>
2023-01-28 10:20:01 +0300

    exercise 18 added

---
+++ es-18/exercise.js
@@ -0,0 +1,9 @@
+let number = 7;
+
+console.log(number > 0 ? "The number is positive" : "The number is negative");
+
+// if (number > 0) {
+
+//   console.log("The number is positive");
+
+// } else console.log("The number is negative");
