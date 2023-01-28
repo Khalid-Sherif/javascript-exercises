eed4a61f97b5f15b3230b8f3a02c710e73a1e2c7
Khaled Sherif <khsherif13@gmail.com>
2023-01-28 11:04:01 +0300

    exercise 20 added

---
+++ es-20/exercise.js
@@ -0,0 +1,9 @@
+let primitive = 1;
+
+if (typeof (primitive) === "number") {
+    console.log(primitive + " : " + typeof (primitive));
+} else if (typeof (primitive) === "string") {
+    console.log(primitive + " : " + typeof (primitive));
+} else {
+    console.log(primitive + " : " + typeof (primitive));
+}
