c6c56df6e48e42ac71454a690f82c968ca5d1cfe
Khaled Sherif <khsherif13@gmail.com>
2023-01-29 22:02:58 +0300

    exercise 46 added

---
+++ es-46/exercise.js
@@ -0,0 +1,12 @@
+const student = {
+  personalData: {
+    name: "John",
+    surname: "Doe",
+    age: "22"
+  },
+  id: 1,
+  school: "Liceo",
+  year: 3,
+};
+
+console.log(student['personalData']);
