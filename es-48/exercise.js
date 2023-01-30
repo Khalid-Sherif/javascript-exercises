67cb8271c1f19fdd8f42906f035f16c32ec78c59
Khaled Sherif <khsherif13@gmail.com>
2023-01-30 13:34:15 +0300

    exercise 48 added

---
+++ es-48/exercise.js
@@ -0,0 +1,18 @@
+let students = [{
+    id: 123,
+    name: 'John',
+    surname: 'Doe',
+    age: 21
+}, {
+    id: 456,
+    name: 'Jane',
+    surname: 'Dope',
+    age: 23
+}, {
+    id: 789,
+    name: 'Kane',
+    surname: 'Lui',
+    age: 26
+}]
+
+console.log(students[0]);
